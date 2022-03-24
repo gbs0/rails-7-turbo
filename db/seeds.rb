c824f8cf4d935e6af80b39f080a8a43fa2cf8da3
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-03-24 12:19:50 -0300

    Adding system test, fixtures and seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ case Rails.env
     puts '-' * 80
     puts 'Removing all Products from database...'
     puts "\n"
-    
+
     Product.destroy_all
 
     puts "Creating Products in Database..."
@@ -21,6 +21,23 @@ case Rails.env
     puts "Done Creating Products!"
 
   when 'test'
+    puts 'Seeding development database'
+    puts '-' * 80
+    puts 'Removing all Products from database...'
+    puts "\n"
+    
+    Product.destroy_all
+
+    puts "Creating Products in Database..."
+  
+    5.times do
+      Product.create!(
+        name: Faker::Commerce.product_name,
+        price: Faker::Commerce.price,
+        quantity: (1..10).to_a.sample
+      )
+    end
+    puts "Done Creating Products!"
 
   when 'production'
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,45 +1,56 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  setup do
+  Rails.application.load_seed
+  
+  setup do 
     @product = products(:one)
+    @product_two = products(:two)
+    @invalid_product = products(:invalid)
   end
 
   test "visiting the index" do
     visit products_url
+
     assert_selector "h1", text: "Products"
   end
 
-  test "should create product" do
+  test "creating a Product" do
     visit products_url
-    click_on "New product"
+    click_on "New Product"
 
     fill_in "Name", with: @product.name
     fill_in "Price", with: @product.price
     fill_in "Quantity", with: @product.quantity
     click_on "Create Product"
 
-    assert_text "Product was successfully created"
+    assert_text "Product was successfully created."
     click_on "Back"
   end
 
-  test "should update Product" do
-    visit product_url(@product)
-    click_on "Edit this product", match: :first
+  test "Showing a Product" do
+    visit products_path
+    click_link @product.name
 
-    fill_in "Name", with: @product.name
-    fill_in "Price", with: @product.price
-    fill_in "Quantity", with: @product.quantity
+    assert_selector "h1", text: @product.name
+  end
+
+  test "updating a Product" do
+    visit products_path
+    assert_selector "h1", text: "Products"
+    
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit Product"
+
+    fill_in "Name", with: @product_two.name
+    fill_in "Price", with: @product_two.price
+    fill_in "Quantity", with: @product_two.quantity
     click_on "Update Product"
 
-    assert_text "Product was successfully updated"
-    click_on "Back"
+    assert_selector "h1", text: "Products"
+    assert_text "Product was successfully updated."
   end
 
-  test "should destroy Product" do
-    visit product_url(@product)
-    click_on "Destroy this product", match: :first
-
-    assert_text "Product was successfully destroyed"
+  test "destroying a Product" do
   end
 end
