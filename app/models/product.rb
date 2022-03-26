6de40792337198e425aa551cb88994080759e2ed
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-26 14:04:08 -0300

    Fix scss files import

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true 
+  validates :price, presence: true
+
+  # Scope for products by name
+  scope :by_name, -> (name) { where("name LIKE ?", "%#{name}%") }
+
+  def self.search(params)
+    Product.all unless params.nil?
+    Product.by_name(params)
+  end
+
 end
