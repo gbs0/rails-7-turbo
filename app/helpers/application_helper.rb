934018c71e9f00f05cd78c9cbdb8dae384d96190
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-03-29 11:00:44 -0300

    Add show_title method in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def self.included(base)
+    base.send :include, InstanceMethods
+  end
+  
+  module InstanceMethods
+    def title(page_title, show_title = true)
+      content_for(:title) { page_title.to_s }
+      @show_title = show_title
+    end
+    
+    def show_title?
+      @show_title
+    end
+  end
 end
+
+
